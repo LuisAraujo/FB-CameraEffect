f184e24226f8ffe48fac255f76e8271f42849840
Luis4raujo <luisaraujo.ifba@gmail>
2017-05-13 23:03:56 -0300

    first commit
    Interface + first effect

---
+++ main.js
@@ -0,0 +1,53 @@
+var imageLoader = document.getElementById('imageLoader');
+imageLoader.addEventListener('change', handleImage, false);
+
+var canvas = document.getElementById('imageCanvas');
+var ctx = canvas.getContext('2d');
+
+
+$('#imageCanvas').click(function(){
+	$("#imageLoader").trigger("click");
+});
+
+var button = document.getElementById('bt-gerate');
+
+button.addEventListener('click', function (e) {
+   
+});
+
+function handleImage(e){
+
+    var reader = new FileReader();
+	
+    reader.onload = function(event){
+	  
+        var img = new Image();
+		
+        img.onload = function(){
+		
+		canvas.width = 400;
+			
+		canvas.height = 400;
+		
+        ctx.drawImage(img,0,0,img.width,img.height,0,0,canvas.width,	canvas.height);
+		  
+		var overimg = new Image();
+		
+		overimg.onload = function(){
+		    ctx.drawImage(overimg, 0, -canvas.height, img.width,img.height, 0, 0, canvas.width, canvas.height);
+		}
+		   
+		   overimg.src = "over-image.png";
+        }
+        
+		img.src = event.target.result;
+		
+		
+		 var dataURL = canvas.toDataURL('image/png');
+		 $("#bt-gerate").attr("href", dataURL);
+		 $("#bt-gerate").css("display","block");
+		
+    }
+	
+    reader.readAsDataURL(e.target.files[0]);     
+}
